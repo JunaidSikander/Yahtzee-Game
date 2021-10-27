fa8a1e748ca447e8e32e2cb315f5e2e5d1b4228f
Junaid Sikander Hakro <junaid_sikander@live.com>
2021-10-27 10:41:40 +0500

    [ADD] game description

--- src/components/RuleRow.js
+++ src/components/RuleRow.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 export default function RuleRow(props) {
-    const {name, score, doScore} = props;
+    const {name, score, description, doScore} = props;
 
     const disabled = score !== undefined;
     const classes = `RuleRow RuleRow-${disabled ? "disabled" : "active"}`;
 
     return (
-        <tr className={classes} onClick={disabled ? null : doScore}>`
+        <tr className={classes} onClick={disabled ? null : doScore}>
             <td className="RuleRow-name">{name}</td>
-            <td className="RuleRow-score">{score}</td>
+            <td className="RuleRow-score">{disabled ? score : description}</td>
         </tr>
     )
 }
